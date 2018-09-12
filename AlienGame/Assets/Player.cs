dcdedf6c196ff6d724e0b988ee5f7c0062ece602
LuisPalominoTrevilla <luispalominot3@gmail.com>
2018-09-11 20:30:28 -0500

    2d game improvements

--- AlienGame/Assets/Player.cs
+++ AlienGame/Assets/Player.cs
@@ -6,10 +6,12 @@ public class Player : MonoBehaviour {
     private Animator animator;
     private SpriteRenderer sr;
     private bool canEnter;
+    private Rigidbody2D rb;
 	// Use this for initialization
 	void Start () {
         this.animator = GetComponent<Animator>();
         this.sr = GetComponent<SpriteRenderer>();
+        this.rb = GetComponent<Rigidbody2D>();
         canEnter = true;
     }
 	
@@ -18,7 +20,7 @@ public class Player : MonoBehaviour {
         float h = Input.GetAxisRaw("Horizontal");
         float speed = h * 5;
         animator.SetFloat("speed", Mathf.Abs(speed));
-        print(canEnter);
+        
         if (speed < 0 && canEnter)
         {
             canEnter = false;
@@ -28,7 +30,21 @@ public class Player : MonoBehaviour {
             canEnter = true;
             this.sr.flipX = false;
         }
+        print(this.animator.GetBool("gun1"));
+        if (Input.GetKeyDown(KeyCode.B) && !this.animator.GetBool("gun1"))
+        {
+            this.animator.SetBool("gun1", true);
+        }
+        else
+        {
+            this.animator.SetBool("gun1", false);
+        }
 
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            this.rb.AddForce(transform.up*10, ForceMode2D.Impulse);
+        }
         transform.Translate(speed*Time.deltaTime, 0, 0);
 	}
 }
