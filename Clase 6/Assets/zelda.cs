73b0b62a52f4c6e53b51090d7f2c5f544b5e0d38
LuisPalominoTrevilla <luispalominot3@gmail.com>
2018-10-09 18:02:26 -0500

    Added Class 6

---
+++ Clase 6/Assets/zelda.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class zelda : MonoBehaviour {
+
+    NavMeshAgent agent;
+    public Camera camera;
+    AudioSource source;
+    public AudioClip[] clipsitos;
+
+	// Use this for initialization
+	void Start () {
+        this.agent = GetComponent<NavMeshAgent>();
+        this.source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray rayito = this.camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(rayito, out hit))
+            {
+                this.agent.destination = hit.point;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            this.source.clip = clipsitos[0];
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            this.source.clip = clipsitos[1];
+        }
+
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            this.source.Play();
+        }
+
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            this.source.Pause();
+        }
+    }
+}
